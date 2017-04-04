5ba0f3c55d0a6412c51c258e457ca901fe95bf79
Unknown <k.ballagher@gmail.com>
2017-04-04 15:28:26 +0100

    Start screen + Story text
    Added initial story text. No interactivity.

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -8,11 +8,20 @@ public class TextController : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		text.text = ("Hello World");
+		text.text = ("Press Space to begin!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.Space)){
+			text.text = ("It was all a horrible mistake. ") +
+						("The evidence was circumstancial at best, but still you ") +
+						("wound up trapped in this prison. Now, it is time to leave.\n\n") +
+						("The cell is damp and empty, save for a bed, it's filthy covers, ") +
+						("and an old mirror on the wall. The door locks from the outside ofcourse, ") +
+						("but there must be some way out of here... \n\n") +
+						("Press S to view the Sheets, M to view the Mirror ") + 
+						("or L to view the Lock.");
+		}
 	}
 }
