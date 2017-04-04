2408b26b29d91eb712a796172596a645eb568d4b
Unknown <k.ballagher@gmail.com>
2017-04-04 15:54:34 +0100

    First state loop
    Added state loop functionality;
    state_cell - state_sheets_0
    
    Minor text adjustments, extra story for sheets_0

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -5,23 +5,49 @@ using System.Collections;
 public class TextController : MonoBehaviour {
 
 	public Text text;
+	private enum States {cell, mirror, sheets_0, sheets_1, lock_0, lock_1, cell_mirror, freedom};
+	private States myState;
 	
 	// Use this for initialization
 	void Start () {
-		text.text = ("Press Space to begin!");
+		myState = States.cell;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)){
+		print (myState);
+		if (myState == States.cell) {
+			state_cell();
+		} else if (myState == States.sheets_0) {
+			state_sheets_0();
+		}
+	}
+	
+	void state_cell () {
 			text.text = ("It was all a horrible mistake. ") +
-						("The evidence was circumstancial at best, but still you ") +
+						("The evidence was circumstancial at best, but you still ") +
 						("wound up trapped in this prison. Now, it is time to leave.\n\n") +
 						("The cell is damp and empty, save for a bed, it's filthy covers, ") +
 						("and an old mirror on the wall. The door locks from the outside ofcourse, ") +
 						("but there must be some way out of here... \n\n") +
 						("Press S to view the Sheets, M to view the Mirror ") + 
 						("or L to view the Lock.");
+			
+			if (Input.GetKeyDown(KeyCode.S)){
+				myState = States.sheets_0;
+			}
+	}
+	
+	void state_sheets_0 () {
+		text.text = ("The dirty sheets are the closest thing to comfort you've ") +
+					("known since arriving here. Sadly there isn't a convenient ") +
+					("window to shimmy down from, and you doubt a single ") +
+					("bedsheet would do the trick regardless.\n\n") +
+					("Press R to return.");
+				
+		
+		if (Input.GetKeyDown(KeyCode.R)){
+			myState = States.cell;
 		}
 	}
 }
