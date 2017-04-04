fa806bfd9271aa82d53d061e8907fa501a38207a
Unknown <k.ballagher@gmail.com>
2017-04-04 16:29:25 +0100

    Additional State Loops
    Added states lock_0, mirror and cell_mirror, along with accompanying text.
    Playable to cell_mirror.

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -20,6 +20,12 @@ public class TextController : MonoBehaviour {
 			state_cell();
 		} else if (myState == States.sheets_0) {
 			state_sheets_0();
+		} else if (myState == States.lock_0) {
+			state_lock_0();
+		} else if (myState == States.mirror) {
+			state_mirror ();
+		} else if (myState == States.cell_mirror) {
+			state_cell_mirror ();
 		}
 	}
 	
@@ -36,11 +42,17 @@ public class TextController : MonoBehaviour {
 			if (Input.GetKeyDown(KeyCode.S)){
 				myState = States.sheets_0;
 			}
+			if (Input.GetKeyDown(KeyCode.L)){
+			myState = States.lock_0;
+			}
+			if (Input.GetKeyDown(KeyCode.M)){
+			myState = States.mirror;
+			}
 	}
 	
 	void state_sheets_0 () {
 		text.text = ("The dirty sheets are the closest thing to comfort you've ") +
-					("known since arriving here. Sadly there isn't a convenient ") +
+					("known since arriving here.\n\nSadly there isn't a convenient ") +
 					("window to shimmy down from, and you doubt a single ") +
 					("bedsheet would do the trick regardless.\n\n") +
 					("Press R to return.");
@@ -48,6 +60,47 @@ public class TextController : MonoBehaviour {
 		
 		if (Input.GetKeyDown(KeyCode.R)){
 			myState = States.cell;
+		}
+	}
+	
+	void state_lock_0 () {
+		text.text = ("Through the cell bars you can just about make out the edge of ") +
+					("the keypad lock.\n\nKnowing how disgusting the guards are here, ") +
+					("you wouldn't be surprised if they left grubby fingerprints all ") +
+					("over the buttons.\n\n") +
+					("Press R to return.");
+		
+		
+		if (Input.GetKeyDown(KeyCode.R)){
+			myState = States.cell;
+		}
+	}
+	void state_mirror () {
+		text.text = ("This grubby old mirror is hanging down at a terrible angle. ") +
+					("It looks as though one of the screws holding it up is long gone. ") +
+					("\n\nYou could probably rip it off the wall easily enough.\n\n") +
+					("Press T to take the mirror, or R to return");
+							
+		
+		if (Input.GetKeyDown(KeyCode.R)){
+			myState = States.cell;
+		}
+		if (Input.GetKeyDown(KeyCode.T)){
+			myState = States.cell_mirror;
+		}
+	}
+	void state_cell_mirror () {
+		text.text = ("The mirror comes off the wall easily, along with copious amounts ") +
+					("of dust. Judging by the wall behind it, this cell was actually ") +
+					("painted at some point.\n\nNot so much now.\n\n") +
+					("Press S to view the Sheets, or L to view the Lock.");
+		
+		
+		if (Input.GetKeyDown(KeyCode.S)){
+			myState = States.sheets_1;
+		}
+		if (Input.GetKeyDown(KeyCode.L)){
+			myState = States.lock_1;
 		}
 	}
 }
