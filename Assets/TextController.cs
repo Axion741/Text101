b2344eeac609253961951f5b8bbba8a63c2da12c
Unknown <k.ballagher@gmail.com>
2017-04-05 13:10:32 +0100

    Section 2 Start
    Added stairs_0, changed freedom to corridor_0.
    Tidy up of if statement formatting.

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class TextController : MonoBehaviour {
 
 	public Text text;
-	private enum States {cell, mirror, sheets_0, sheets_1, lock_0, lock_1, cell_mirror, freedom};
+	private enum States {cell, mirror, sheets_0, sheets_1, lock_0, lock_1, cell_mirror, corridor_0, stairs_0};
 	private States myState;
 	
 	// Use this for initialization
@@ -16,26 +16,19 @@ public class TextController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		print (myState);
-		if (myState == States.cell) {
-			state_cell();
-		} else if (myState == States.sheets_0) {
-			state_sheets_0();
-		} else if (myState == States.lock_0) {
-			state_lock_0();
-		} else if (myState == States.mirror) {
-			state_mirror ();
-		} else if (myState == States.cell_mirror) {
-			state_cell_mirror ();
-		} else if (myState == States.sheets_1) {
-			state_sheets_1 ();
-		} else if (myState == States.lock_1) {
-			state_lock_1 ();
-		}else if (myState == States.freedom) {
-			state_freedom ();
-		}
+		//Section 1 States
+		if 		(myState == States.cell) 		{cell();} 
+		else if (myState == States.sheets_0) 	{sheets_0();}
+		else if (myState == States.lock_0) 		{lock_0();}
+		else if (myState == States.mirror) 		{mirror ();} 
+		else if (myState == States.cell_mirror) {cell_mirror ();} 
+		else if (myState == States.sheets_1) 	{sheets_1 ();} 
+		else if (myState == States.lock_1) 		{lock_1 ();}
+		else if (myState == States.corridor_0) 	{corridor_0 ();}
+		else if (myState == States.stairs_0)	{stairs_0 ();}
 	}
-	
-	void state_cell () {
+	//Section 1 - Cell to Corridor
+	void cell () {
 			text.text = ("It was all a horrible mistake. ") +
 						("The evidence was circumstancial at best, but you still ") +
 						("wound up trapped in this prison. Now, it is time to leave.\n\n") +
@@ -45,18 +38,12 @@ public class TextController : MonoBehaviour {
 						("Press S to view the Sheets, M to view the Mirror ") + 
 						("or L to view the Lock.");
 			
-			if (Input.GetKeyDown(KeyCode.S)){
-				myState = States.sheets_0;
-			}
-			if (Input.GetKeyDown(KeyCode.L)){
-			myState = States.lock_0;
-			}
-			if (Input.GetKeyDown(KeyCode.M)){
-			myState = States.mirror;
-			}
+			if (Input.GetKeyDown(KeyCode.S))	{myState = States.sheets_0;}
+			if (Input.GetKeyDown(KeyCode.L))	{myState = States.lock_0;}
+			if (Input.GetKeyDown(KeyCode.M))	{myState = States.mirror;}
 	}
 	
-	void state_sheets_0 () {
+	void sheets_0 () {
 		text.text = ("The dirty sheets are the closest thing to comfort you've ") +
 					("known since arriving here.\n\nSadly there isn't a convenient ") +
 					("window to shimmy down from, and you doubt a single ") +
@@ -64,12 +51,10 @@ public class TextController : MonoBehaviour {
 					("Press R to return.");
 				
 		
-		if (Input.GetKeyDown(KeyCode.R)){
-			myState = States.cell;
-		}
+		if (Input.GetKeyDown(KeyCode.R))	{myState = States.cell;}
 	}
 	
-	void state_lock_0 () {
+	void lock_0 () {
 		text.text = ("Through the cell bars you can just about make out the edge of ") +
 					("the keypad lock.\n\nKnowing how disgusting the guards are here, ") +
 					("you wouldn't be surprised if they left grubby fingerprints all ") +
@@ -77,68 +62,68 @@ public class TextController : MonoBehaviour {
 					("Press R to return.");
 		
 		
-		if (Input.GetKeyDown(KeyCode.R)){
-			myState = States.cell;
-		}
+		if (Input.GetKeyDown(KeyCode.R))	{myState = States.cell;}
 	}
-	void state_mirror () {
+	
+	void mirror () {
 		text.text = ("This grubby old mirror is hanging down at a terrible angle. ") +
 					("It looks as though one of the screws holding it up is long gone. ") +
 					("\n\nYou could probably rip it off the wall easily enough.\n\n") +
 					("Press T to take the mirror, or R to return");
 							
 		
-		if (Input.GetKeyDown(KeyCode.R)){
-			myState = States.cell;
-		}
-		if (Input.GetKeyDown(KeyCode.T)){
-			myState = States.cell_mirror;
-		}
+		if (Input.GetKeyDown(KeyCode.R))	{myState = States.cell;}
+		if (Input.GetKeyDown(KeyCode.T))	{myState = States.cell_mirror;}
 	}
-	void state_cell_mirror () {
+	
+	void cell_mirror () {
 		text.text = ("The mirror comes off the wall easily, along with copious amounts ") +
 					("of dust. Judging by the wall behind it, this cell was actually ") +
 					("painted at some point.\n\nNot so much now.\n\n") +
 					("Press S to view the Sheets, or L to view the Lock.");
 		
 		
-		if (Input.GetKeyDown(KeyCode.S)){
-			myState = States.sheets_1;
-		}
-		if (Input.GetKeyDown(KeyCode.L)){
-			myState = States.lock_1;
-		}
+		if (Input.GetKeyDown(KeyCode.S)) {myState = States.sheets_1;}
+		if (Input.GetKeyDown(KeyCode.L)) {myState = States.lock_1;}
 	}
-	void state_sheets_1 () {
+	
+	void sheets_1 () {
 		text.text = ("You try to wipe the dust away from the mirror with ") +
 					("limited success. But hey, those sheets are dirtier now.\n\n") +
 					("Press R to return.");
 		
 		
-		if (Input.GetKeyDown(KeyCode.R)){
-			myState = States.cell_mirror;
-		}
-		
+		if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell_mirror;}
 	}
-	void state_lock_1 () {
+	
+	void lock_1 () {
 		text.text = ("It's not terribly comfortable, but you can just stretch enough ") +
 					("to see the reflection of the worn keypad. Three numbers stand out ") +
 					("as particularly well used.\n\n") +
 					("Press O to try to open the cell or press R to return.");
 		
 		
-		if (Input.GetKeyDown(KeyCode.R)){
-			myState = States.cell_mirror;
-		}
-		if (Input.GetKeyDown(KeyCode.O)){
-			myState = States.freedom;
-		}
-		
+		if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell_mirror;}
+		if (Input.GetKeyDown(KeyCode.O)) {myState = States.corridor_0;}
 	}
-	void state_freedom () {
+		
+	//Section 2 - Corridor to Courtyard
+	void corridor_0 () {
 		text.text = ("After a few failed attempts a loud buzz permeates the cell. ") +
 					("The door swings open under your weight as you stumble into the ") +
-					("corridor.\n\n");
-		}
+					("corridor.\n\nTo your left is door with the word 'Janitor' on ") +
+					("a faded sign. The corridor ends in staircase leading up.\n\n") +
+					("Press S to look up the Stairs, C to try the closet door or F to ") +
+					("look around on the corridor.");
 					
+		if (Input.GetKeyDown(KeyCode.S)){myState = States.stairs_0;}
+	}
+	void stairs_0 () {
+		text.text = ("A steep narrow staircase leads up to a heavy steel door. ") +
+					("Through the small window, you can see the blurred shadows ") +
+					("of staff walking by.\n\nPress R to return.");
+					
+		
+		if (Input.GetKeyDown(KeyCode.R)){myState = States.corridor_0;}
+	}				
 }
